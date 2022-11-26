4fc612c18423caf260dfc89df1a87a73169e96dc
gerinmordekhai <mordekhaigerinlumangkun@gmail.com>
2022-11-26 20:29:38 +0700

    features: update carousel transition

--- src/components/landingPage/Carousel.jsx
+++ src/components/landingPage/Carousel.jsx
@@ -32,16 +32,20 @@ const Carousel = ({ content }) => {
 
   console.log("karusel", content);
   return (
-    <div className="relative w-full h-[640px] rounded-xl shadow-lg">
-      <div className="absolute text-[40px] top-2/4 bg-natural flex justify-center items-center w-[40px] h-[40px] rounded-full cursor-pointer left-[15px] hover:bg-slate-400 hover:scale-110 duration-300" onClick={prev}>
+    <div className="relative w-full h-[640px] rounded-xl">
+      <div className="absolute text-[40px] top-2/4 bg-natural flex justify-center items-center w-[40px] h-[40px] rounded-full cursor-pointer left-[15px] hover:bg-slate-400 hover:scale-110 duration-300 z-40" onClick={prev}>
         &lsaquo;
       </div>
-      <div className="absolute text-[40px] top-2/4 bg-natural flex justify-center items-center w-[40px] h-[40px] rounded-full cursor-pointer right-[15px] hover:bg-slate-400 hover:scale-110 duration-300" onClick={next}>
+      <div className="absolute text-[40px] top-2/4 bg-natural flex justify-center items-center w-[40px] h-[40px] rounded-full cursor-pointer right-[15px] hover:bg-slate-400 hover:scale-110 duration-300 z-40" onClick={next}>
         &rsaquo;
       </div>
 
       {content.map((slide, i) => {
-        return <div key={i}>{i === imgIndex && <img src={slide.imgUrl} alt="carousel" className="absolute object-cover w-full h-[640px] rounded-xl -z-10" />}</div>;
+        return (
+          <div className={i === imgIndex ? "slide active" : "slide"} key={i}>
+            {i === imgIndex && <img src={slide.imgUrl} alt="carousel" className="absolute object-cover w-full h-[640px] rounded-xl -z-10" />}
+          </div>
+        );
       })}
     </div>
   );
